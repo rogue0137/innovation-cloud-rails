30950e1e5dd3a17c0309ff74a99803792025e83e
Krystal Flores <krystal.flores@gmail.com>
2015-01-31 12:49:55 -0800

    Finished project according to codecademy outline

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'thanks' => 'pages#thanks'
+  resources :signups
+  
+  root 'signups#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
